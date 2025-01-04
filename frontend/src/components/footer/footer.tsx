4d4a5388ffdb0b1a7c67ee48bd99682edbe9a1df
Silje Melling <silje.melling94@gmail.com>
2025-01-04 21:43:07 +0100

    create header + footer

---
+++ frontend/src/components/footer/footer.tsx
@@ -0,0 +1,9 @@
+import styles from "../styles/footer.module.css";
+
+export default function Footer() {
+    return (
+    <footer className={styles.footer}>
+    <div>You have completed 5/10 tasks today, way to go!</div>
+    </footer>
+    );
+}
